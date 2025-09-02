2a62579dbbc915e3450a0787df2135ca910b235c
Giulio Attenni <46457883+attennig@users.noreply.github.com>
2025-09-02 16:16:36 +0100

    Update tokens.py

--- src/tokens.py
+++ src/tokens.py
@@ -1,2 +1,2 @@
-ELECTRICITYMAPS_API_TOKEN = "oxXFjh6cXPnPd" # https://www.electricitymaps.com/pricing
+ELECTRICITYMAPS_API_TOKEN = "" # https://www.electricitymaps.com/pricing
 VISUALCROSSING_API_TOKEN = "HC8KHZDX7BZDRZVMH5GCRXBT8"  # https://www.visualcrossing.com/weather-api/
